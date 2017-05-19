dfd703edeee7e85d343665512362169645199f07
Muhammad Faishol Amirul Mukminin <faishol.amirul27@gmail.com>
2017-05-19 17:33:35 +0700

    Create guess.judge.cpp

---
+++ examples/interaktif/guess.judge.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+#include <fstream>
+using namespace std;
+
+ofstream vd("verdict");
+
+int WA(){
+	vd << "Verdict: Wrong Answer\n";
+	return 0;
+}
+
+int AC(){
+	vd << "Verdict: Accepted\n";
+	return 0;
+}
+
+int TLE(){
+	vd << "Verdict: Time Limit Excedeed\n";
+	return 0;
+}
+
+int main(){
+  int L, R, K, ans;
+  L = 1;
+  R = 10;
+  K = 10;
+  ans = 8;
+  
+  cout << L << " " << R << " " << K << endl;
+  fflush(stdout);
+  
+  for(int i=1;i<=K;i++){
+    int id, angka;
+    
+    cin >> id >> angka;
+    if(id == 0){
+      if(angka == ans) cout << "YA\n";
+      else cout << "TIDAK\n";
+      
+      fflush(stdout);
+      
+    }else{
+      if(angka == ans) return AC();
+      else return WA();
+    }
+  }
+  
+  return TLE();
+}
